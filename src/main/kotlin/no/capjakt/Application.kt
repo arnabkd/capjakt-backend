83a4d20b372a6ec0815d3fb6c182af90d0e1f10d
Arnab Kumar Datta <arnabkum@gmail.com>
2021-12-02 11:25:21 +0100

    first commit

---
+++ src/main/kotlin/no/capjakt/Application.kt
@@ -0,0 +1,14 @@
+package no.capjakt
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import no.capjakt.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+        configureMonitoring()
+        configureHTTP()
+    }.start(wait = true)
+}
