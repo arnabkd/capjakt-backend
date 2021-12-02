5b51b51e0d2893f4634ab6102b27826baa482ec8
Arnab Kumar Datta <arnabkum@gmail.com>
2021-12-02 15:38:30 +0100

    log jdbc and db credentials

--- src/main/kotlin/no/capjakt/plugins/Monitoring.kt
+++ src/main/kotlin/no/capjakt/plugins/Monitoring.kt
@@ -2,6 +2,7 @@ package no.capjakt.plugins
 
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.application.log
 import io.ktor.features.CallLogging
 import io.ktor.request.path
 import org.slf4j.event.Level
@@ -11,4 +12,10 @@ fun Application.configureMonitoring() {
     level = Level.INFO
     filter { call -> call.request.path().startsWith("/") }
   }
+
+  val jdbcUrl = System.getenv("JDBC_DATABASE_URL")
+  log.debug("jdbc url is $jdbcUrl")
+
+  val dbUrl = System.getenv("DATABASE_URL")
+  log.debug("db url is $dbUrl")
 }
